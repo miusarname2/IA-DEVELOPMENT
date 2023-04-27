c3c8a8938787ac050be5d1569a670d55cfdeeff1
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-04-27 00:19:04 -0500

    SE agregaron comentarios pertinentes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import openai, sys, json, os
 # key provider
 def keyProvider():
     try:
-        key = "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"
+        key = "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"
         openai.api_key = key
         return key
     except KeyError:
@@ -51,75 +51,107 @@ def chatbot(peticion, e=0):
 
 #Clase generadora de interface
 
+# Definición de la clase imageApp que hereda de ft.UserControl
 class imageApp(ft.UserControl):
+    
+    # Método que construye la aplicación
     def build(self):
+        # Crear un cuadro de texto para ingresar solicitudes
         self.new_request = ft.TextField(hint_text="Whats needs to be done?", expand=True)
+        # Crear una columna para mostrar las solicitudes y las imágenes generadas
         self.request = ft.Column()
 
-        # application's root control (i.e. "view") containing all other controls
+        # Devolver una columna que contiene todos los controles necesarios para la aplicación
         return ft.Column(
             width=600,
             controls=[
+                # Crear una fila que contiene el cuadro de texto y el botón para enviar solicitudes
                 ft.Row(
                     controls=[
                         self.new_request,
                         ft.FloatingActionButton("Send", on_click=self.add_rq),
                     ],
                 ),
+                # Agregar la columna para mostrar las solicitudes y las imágenes generadas
                 self.request,
             ],
         )
 
+    # Método que se llama cuando se agrega una nueva solicitud
     def add_rq(self, e):
-        # self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        # Crear un nuevo control de texto con la solicitud ingresada por el usuario
+        # y agregarlo a la columna de solicitudes
         texto1 = ""
         texto = "You: \n", self.new_request.value
         for i in texto:
             texto1 += i
         self.request.controls.append(ft.Text(texto1))
-        print(self.new_request.value)
-        texto = ft.Text("PixelAi: "), ft.Image(src=f"{imageGenerator(self.new_request.value)}",width=600)
-        texto1 = ""
+        
+        # Generar una imagen a partir de la solicitud ingresada por el usuario
+        imagen_generada = imageGenerator(self.new_request.value)
+        
+        # Crear un nuevo control de texto con el nombre del generador de imágenes (PixelAi)
+        # y un nuevo control de imagen con la imagen generada, y agregar ambos a la columna de solicitudes
+        texto = ft.Text("PixelAi: "), ft.Image(src=f"{imagen_generada}",width=600)
         for i in texto:
             self.request.controls.append(i)
+        
+        # Limpiar el cuadro de texto para ingresar nuevas solicitudes
         self.new_request.value = ""
+        # Actualizar la aplicación para mostrar las nuevas solicitudes y las imágenes generadas
         self.update()
 
 
-#clase generadoras de chatbot interface
+
+# Definición de la clase TodoApp que hereda de ft.UserControl
 class TodoApp(ft.UserControl):
+    
+    # Método que construye la aplicación
     def build(self):
+        # Crear un cuadro de texto para ingresar nuevas tareas
         self.new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
+        # Crear una columna para mostrar las tareas agregadas
         self.tasks = ft.Column()
 
-        # application's root control (i.e. "view") containing all other controls
+        # Devolver una columna que contiene todos los controles necesarios para la aplicación
         return ft.Column(
             width=600,
             controls=[
+                # Crear una fila que contiene el cuadro de texto y el botón para agregar tareas
                 ft.Row(
                     controls=[
                         self.new_task,
                         ft.FloatingActionButton("Send", on_click=self.add_request),
                     ],
                 ),
+                # Agregar la columna para mostrar las tareas
                 self.tasks,
             ],
         )
 
+    # Método que se llama cuando se agrega una nueva tarea
     def add_request(self, e):
-        # self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        # Crear un nuevo control de texto con la tarea ingresada por el usuario
+        # y agregarlo a la columna de tareas
         texto1 = ""
         texto = "You: \n", self.new_task.value
         for i in texto:
             texto1 += i
         self.tasks.controls.append(ft.Text(texto1))
+        
+        # Crear un nuevo control de texto con la respuesta del ChatBot a la tarea ingresada
+        # y agregarlo a la columna de tareas
         texto = "ChatBot: ", chatbot(self.new_task.value)
         texto1 = ""
         for i in texto:
             texto1 += i
         self.tasks.controls.append(ft.Text(texto1))
+        
+        # Limpiar el cuadro de texto para ingresar nuevas tareas
         self.new_task.value = ""
+        # Actualizar la aplicación para mostrar las nuevas tareas agregadas
         self.update()
+
 
 #main
 def main(page: ft.Page):
